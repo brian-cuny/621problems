d1a8601a711245fe6461f725ef4a8f15a5bbd807
brian.weinfeld+cuny@gmail.com <brian.weinfeld+cuny@gmail.com>
2018-05-25 16:33:41 -0400

    marr_chapter 2

--- chatper2_marr.R
+++ chatper2_marr.R
@@ -58,6 +58,87 @@ predict(blm, newdata, interval='predict')
 #This prediction is not a terrible general guideline for quick, simple calculations. Our regression model determined that a week could expected
 #about 98% of the business of the previous week. For a quick approximation, 100% is close enough to 98%
 
+#2
+
+indicators <- read_delim(file='C:\\Users\\Brian\\Desktop\\GradClasses\\Summer18\\621\\621problems\\marr\\indicators.txt', delim = '\t')
+
+ilm <- lm(PriceChange ~ LoanPaymentsOverdue, indicators)
+
+summary(ilm)
+
+ggplot(ilm) +
+  geom_point(aes(LoanPaymentsOverdue, PriceChange))
+
+#y = -2.2485x+4.5145
+
+#a
+-2.2485 + 0.9033*2.472878
+-2.2485 - 0.9033*2.472878
+
+#The confidence interval is from -4.482251 to -0.0147493 There is evidence to suggest the slope is statistically significant. This is because the slope does
+#not include 0
+
+#b
+-2.2485*4+4.5145
+dat <- data_frame(LoanPaymentsOverdue = 4)
+
+predict(ilm, dat, interval='confidence')
+
+#No 95% is not reasonable. It can be stated with 95% confidence that the regression slope in contained in -6.648849 and -2.310322
+
+#3
+
+#a
+
+# b0 = 0.6417099 se = 0.1222707 t-value 2.368452
+
+0.6417099 + 0.1222707*2.368452
+0.6417099 - 0.1222707*2.368452
+
+#0.3521176 - 0.9313022
+
+
+#b
+# b1 = 0.0112916 se = 0.0008184 t-value 2.368452
+
+0.0112916 + 0.0008184*2.368452
+0.0112916 - 0.0008184*2.368452
+
+# 0.009353259 - 0.01322994
+
+# We fail to reject the null hypothesis. There is evidence to suggest that the slope is equal to 0.01
+
+#c
+
+0.0112916*130+0.6417099 #2.109618
+
+2.1092618 + 2.368452*0.0008184*sqrt(1 + 1/30 + 0)
+2.1092618 - 2.368452*0.0008184*sqrt(1 + 1/30 + 0)
+
+# 2.107291 - 2.111232
+
+#5
+
+#D. RSS measures the size of the error in the regression line. That is, the amount of variability not explained by the model. The second model has
+# significantly more variability in this regard. Conversly, SSreg indicates the variability from the model. The variability in the model is much smaller than
+#that of model 2.
+
+#7
+
+#The true slope of the regression line (that is, of the population) is unknown. The slope of the regression line as plotted is our best guess as to that slope
+#The confidence interval indicates the range of values that the true slope could potentially be. However, this does not mean that all points should fall inside
+#this range. Any given single point could potentially be significantly higher or lower due to error and thus fall out of the range of the confidence interval
+#for the regression line. We are in essence making a prediction based on a prediction. That is, the regression slope could be any of a range of value and thus
+#any given point could be in an even larger range of values as it must encompass all potential range of values for the regression line.
+
+
+
+
+
+
+
+
+
 
 
 
